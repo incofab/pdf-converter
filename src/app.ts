59b714695741c80afd7ca4b1bbea5ecd63d940e7
Incofab <incofabikenna@gmail.com>
2025-12-23 01:05:26 +0100

    html to pdf

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
 import * as middlewares from "./middlewares";
 import api from "./api";
 import MessageResponse from "./interfaces/MessageResponse";
-import path from "path";
+import { pdfOutputPath } from "./util/util";
+import fs from "fs";
 
 const app = express();
 
@@ -14,9 +15,15 @@ app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "10mb" }));
-// app.use(express.json());
 
-app.use("/pdfs", express.static(path.join(__dirname, "pdfs")));
+// app.use("/pdfs", express.static(path.join(__dirname, "pdfs")));
+app.get("/pdfs/:file", (req, res) => {
+  const filePath = pdfOutputPath(req.params.file);
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send("File not found");
+  }
+  res.download(filePath);
+});
 
 app.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
