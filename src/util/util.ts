17feb44689a01e7c5e8743dce15b1897eb948eea
mac <mac@macs-MacBook-Pro.local>
2026-03-14 11:46:34 +0100

    dockerize

--- src/util/util.ts
+++ src/util/util.ts
@@ -5,28 +5,44 @@ import express from "express";
 
 export async function getBrowser() {
   return await puppeteer.launch({
-    headless: process.env.NODE_ENV !== "development",
+    headless: isProduction(),
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage", // optional, helps with /dev/shm issues
+      "--single-process",
+      "--no-zygote",
     ],
+    executablePath: isProduction()
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath(),
     timeout: 60_000, // 60 seconds
   });
 }
 
 export const pdfFolder = path.join(__dirname, "../pdfs");
+
 export function pdfOutputPath(filename: string) {
   if (!fs.existsSync(pdfFolder)) {
     fs.mkdirSync(pdfFolder);
   }
   return path.join(pdfFolder, filename);
 }
+
 export function baseUrl(req: express.Request) {
   return `${req.protocol}://${req.get("host")}`;
 }
+
 export function pdfFileUrl(req: express.Request, filename: string) {
   return `${baseUrl(req)}/pdfs/${filename}`;
+}
+
+function isProduction() {
+  return process.env.NODE_ENV === "production";
+}
+
+function isDevelopment() {
+  return process.env.NODE_ENV === "development";
 }
 
 export async function deleteOldPdfFiles() {
